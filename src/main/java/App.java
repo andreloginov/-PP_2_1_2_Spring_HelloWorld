7abbf79c08a4054d249a122729cab6569a3aa8f4
maksvaliullin <117544522+maksvaliullin@users.noreply.github.com>
2022-12-14 22:42:48 +0300

    first

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat1 = applicationContext.getBean("someCat", Cat.class);
+        Cat cat2 = applicationContext.getBean("someCat", Cat.class);
+
+        System.out.println(bean == bean2);
+        System.out.println(cat1 == cat2);
+
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,35 @@
+import java.util.Objects;
+
+public class Cat {
+private String value;
+    public Cat() {
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(value, cat.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
